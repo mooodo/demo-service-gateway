db29dc43c4daec146805d71dbe7018ada4836043
mooodo <fangang2004@126.com>
2019-08-12 17:10:43 +0800

    commit all

---
+++ src/main/java/com/demo/gateway/GatewayApplication.java
@@ -0,0 +1,23 @@
+/* 
+ * Created by 2019年7月1日
+ */
+package com.demo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author fangang
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
